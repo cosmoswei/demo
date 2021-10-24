cf639e0418908414d93a20c0b78cb01adc1b6e91
cosmoswei <2117008741@qq.com>
2021-10-24 19:45:56 +0800

    Initial commit

--- task/src/main/java/com/wei/test/Test.java
+++ task/src/main/java/com/wei/test/Test.java
@@ -6,5 +6,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Git Test");
         System.out.println(System.currentTimeMillis());
+        System.out.println(Thread.currentThread().getName());
     }
 }
