44b0354303b00cab26e7019b64d8c157f9e4626a
cosmoswei <2117008741@qq.com>
2021-10-24 19:47:45 +0800

    delete：删除了一行

--- task/src/main/java/com/wei/test/Test.java
+++ task/src/main/java/com/wei/test/Test.java
@@ -6,6 +6,5 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Git Test");
         System.out.println(System.currentTimeMillis());
-        System.out.println(Thread.currentThread().getName());
     }
 }
