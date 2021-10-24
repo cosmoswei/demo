69f3edb206f1889203d715633c03327cb1f10e4d
cosmoswei <2117008741@qq.com>
2021-10-24 19:50:06 +0800

    add：增加的一行

--- task/src/main/java/com/wei/test/Test.java
+++ task/src/main/java/com/wei/test/Test.java
@@ -6,5 +6,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Git Test");
         System.out.println(System.currentTimeMillis());
+        System.out.println(System.currentTimeMillis());
+        System.out.println(System.currentTimeMillis());
     }
 }
