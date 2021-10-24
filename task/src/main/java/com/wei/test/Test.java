d9f7bcbfcf70006d2b55d0ad2423e4bdb2661c8d
cosmoswei <2117008741@qq.com>
2021-10-24 19:44:42 +0800

    Initial commit

--- task/src/main/java/com/wei/test/Test.java
+++ task/src/main/java/com/wei/test/Test.java
@@ -1,7 +1,10 @@
 package com.wei.test;
 
+import java.util.concurrent.TimeUnit;
+
 public class Test {
     public static void main(String[] args) {
         System.out.println("Git Test");
+        System.out.println(System.currentTimeMillis());
     }
 }
